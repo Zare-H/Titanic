2c5837de63e5c40aa71fbf0fd054a3628e2a4d7e
Zare-H <hassan.zareh.abc@gmail.com>
2025-08-12 21:53:14 -0500

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='titanic-streamlit-app',
+    name='my_project_name',  # Change this
     version='0.1.0',
-    description='A Streamlit app for Titanic data analysis and prediction',
-    author='Your Name',
-    author_email='your.email@example.com',
     packages=find_packages(),
     install_requires=[
-        'streamlit',
+        # List your dependencies here, e.g.:
         'pandas',
         'numpy',
-        'scikit-learn',
-        'matplotlib',
-        'altair',
     ],
-    license='Apache License 2.0',
     classifiers=[
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
 )
